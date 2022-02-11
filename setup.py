b3bda405029f6202071c3d82f3015e34cfa90233
Alve Svarén <alve@hotmail.se>
2022-02-11 12:46:32 +0100

    New version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md") as file:
 
 setuptools.setup(
     name="latex-render",
-    version="0.0.1",
+    version="0.0.2",
     author="Alve Svarén",
     author_email="alve@hotmail.se",
     description="A simple library and cli to render latex code to images",
