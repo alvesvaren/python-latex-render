04b9c4eff5253540ae32e9e0d0375d6239ee2475
Alve Svarén <alve@hotmail.se>
2022-02-11 12:43:39 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md") as file:
+    long_description = file.read()
+
+setuptools.setup(
+    name="latex-render",
+    version="0.0.1",
+    author="Alve Svarén",
+    author_email="alve@hotmail.se",
+    description="A simple library and cli to render latex code to images",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/alvesvaren/mccli",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires='>=3.6',
+)
